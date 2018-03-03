fd2ca7458cfca13001d84d7788ffd06123985c52
Stuart Harris <stuart.harris@red-badger.com>
2018-03-03 14:11:47 +0000

    listen for PR webhook (WIP)

---
+++ deploy.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"gopkg.in/go-playground/webhooks.v3"
+	"gopkg.in/go-playground/webhooks.v3/github"
+)
+
+const (
+	secretEnvVar = "DEPLOY_SECRET"
+	path         = "/webhooks"
+	port         = 3016
+)
+
+func main() {
+	secret, ok := os.LookupEnv(secretEnvVar)
+	if ok == false {
+		log.Fatalf("Environment variable %s is not exported.", secretEnvVar)
+	}
+
+	hook := github.New(&github.Config{Secret: secret})
+	hook.RegisterEvents(handlePullRequest, github.PullRequestEvent)
+
+	err := webhooks.Run(hook, ":"+strconv.Itoa(port), path)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handlePullRequest(payload interface{}, header webhooks.Header) {
+	fmt.Println("Handling PR")
+	pl := payload.(github.PullRequestPayload)
+
+	fmt.Printf("PR #%d, SHA %s\n", pl.PullRequest.Number, pl.PullRequest.Head.Sha)
+}
