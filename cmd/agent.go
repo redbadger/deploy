5b2dee0015aba8c80a5695062b00f2c3c712c472
Stuart Harris <stuart.harris@red-badger.com>
2018-03-24 06:46:20 +0000

    more idiomatic use of cobra

--- cmd/agent.go
+++ cmd/agent.go
@@ -9,11 +9,17 @@ import (
 	"github.com/spf13/viper"
 )
 
+var (
+	path string
+	port uint16
+)
+
 var agentCmd = &cobra.Command{
-	Use:   "agent",
-	Short: "Run deploy in agent mode",
+	Use:     "agent",
+	Aliases: []string{"daemon", "bot"},
+	Short:   "Run deploy as an agent",
 	Long: `
-Run deploy in agent mode:
+Run deploy as an agent:
 
 1.  watches for PR updates on a webhook
 2.  clones the repo to an in-memory filesystem
@@ -22,16 +28,8 @@ Run deploy in agent mode:
 5.  gathers yaml files (however they are nested)
 6.  applies the manifests to a Kubernetes cluster using kubctl.
 `,
+	Example: `deploy agent &`,
 	Run: func(cmd *cobra.Command, args []string) {
-		port, err := cmd.Flags().GetUint16("port")
-		if err != nil {
-			log.Fatalf("Must specifiy port: %v", err)
-		}
-		path, err := cmd.Flags().GetString("path")
-		if err != nil {
-			log.Fatalf("Must specifiy path: %v", err)
-		}
-
 		if !viper.IsSet(constants.SecretEnvVar) {
 			log.Fatalf("environment variable %s is not exported.\n", constants.SecretEnvVar)
 		}
@@ -48,6 +46,6 @@ Run deploy in agent mode:
 
 func init() {
 	rootCmd.AddCommand(agentCmd)
-	agentCmd.Flags().Uint16P("port", "p", 3016, "Port for webhook listener")
-	agentCmd.Flags().String("path", "/webhooks", "Path for webhook url")
+	agentCmd.Flags().Uint16VarP(&port, "port", "p", 3016, "Port for webhook listener")
+	agentCmd.Flags().StringVar(&path, "path", "/webhooks", "Path for webhook url")
 }
