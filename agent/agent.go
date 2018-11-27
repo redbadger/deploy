179ee2c4c25e1436e385a60a17cce7efa666ece6
Stuart Harris <stuart.harris@red-badger.com>
2018-11-27 14:44:44 +0000

    use webhooks.v5 and add health endpoint

--- agent/agent.go
+++ agent/agent.go
@@ -1,35 +1,23 @@
 package agent
 
 import (
+	"net/http"
 	"strconv"
 
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/go-playground/webhooks.v3"
-	webhook "gopkg.in/go-playground/webhooks.v3/github"
+	"gopkg.in/go-playground/webhooks.v5/github"
 
 	"github.com/redbadger/deploy/model"
 )
 
-type logrusWebhookLogger struct{}
-
-func (l *logrusWebhookLogger) Info(msg string) {
-	log.Info(msg)
-}
-
-func (l *logrusWebhookLogger) Error(msg string) {
-	log.Error(msg)
-}
-
-func (l *logrusWebhookLogger) Debug(msg string) {
-	log.Debug(msg)
-}
-
 // Agent runs deploy as a bot
 func Agent(port uint16, path, token, secret string) {
-	hook := webhook.New(&webhook.Config{Secret: secret})
-	hook.RegisterEvents(createWebhookHandler(token), webhook.PullRequestEvent)
+	http.HandleFunc("/webhooks", createWebhookHandler(secret, token))
+	http.HandleFunc("/healthz", createHealthHandler())
 
-	err := webhooks.Run(hook, ":"+strconv.FormatUint(uint64(port), 10), path)
+	address := ":" + strconv.FormatUint(uint64(port), 10)
+	log.Infof("Listening on address %s", address)
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.WithError(err).Fatal("listening for webhook")
 	}
@@ -44,31 +32,54 @@ func consume(ch chan *model.DeploymentRequest) {
 	}
 }
 
-func createWebhookHandler(token string) func(interface{}, webhooks.Header) {
-	webhooks.DefaultLog = new(logrusWebhookLogger)
-
+func createWebhookHandler(secret, token string) http.HandlerFunc {
 	ch := make(chan *model.DeploymentRequest, 100)
 	go consume(ch)
-	return func(payload interface{}, header webhooks.Header) {
-		pl := payload.(webhook.PullRequestPayload)
-		pr := pl.PullRequest
-		myLog := log.WithFields(log.Fields{
-			"action":      pl.Action,
-			"pullRequest": pr.Number,
-		})
-		switch pl.Action {
-		case "opened", "synchronize":
-			myLog.WithField("sha", pr.Head.Sha).Info("actioning webhook")
-			ch <- &model.DeploymentRequest{
-				URL:      pl.Repository.URL,
-				CloneURL: pl.Repository.CloneURL,
-				Token:    token,
-				Owner:    pl.Repository.Owner.Login,
-				Repo:     pl.Repository.Name,
-				Number:   pr.Number,
+
+	hook, _ := github.New(github.Options.Secret(secret))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				http.Error(w, "only 'pull_request' events are supported", http.StatusNotAcceptable)
+			} else {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			return
+		}
+
+		switch pl := payload.(type) {
+		case github.PullRequestPayload:
+			pr := pl.PullRequest
+			myLog := log.WithFields(log.Fields{
+				"action":      pl.Action,
+				"pullRequest": pr.Number,
+			})
+
+			switch pl.Action {
+			case "opened", "synchronize":
+				myLog.WithField("sha", pr.Head.Sha).Info("actioning webhook")
+				ch <- &model.DeploymentRequest{
+					URL:      pl.Repository.URL,
+					CloneURL: pl.Repository.CloneURL,
+					Token:    token,
+					Owner:    pl.Repository.Owner.Login,
+					Repo:     pl.Repository.Name,
+					Number:   pr.Number,
+				}
+			default:
+				myLog.Info("webhook ignored")
 			}
 		default:
-			myLog.Info("webhook ignored")
+			log.Info("payload not supported")
 		}
+	}
+}
+
+func createHealthHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
 	}
 }
